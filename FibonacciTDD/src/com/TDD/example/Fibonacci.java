550fbb5a093a4c9a5688f07b5c7a4c9f6a6fdaec
Grzesiek19 <grzesyou@wp.pl>
2015-06-06 16:05:48 +0200

    Passed test for Fibonacci value for term=5

--- FibonacciTDD/src/com/TDD/example/Fibonacci.java
+++ FibonacciTDD/src/com/TDD/example/Fibonacci.java
@@ -3,22 +3,30 @@ package com.TDD.example;
 public class Fibonacci {
 
 	private int term;
-	
+
 	public Fibonacci() {
 		this.term = term;
 		// TODO Auto-generated constructor stub
 	}
 
 	public int getFiboValue(int term) {
-		if( term == 0){
+		int fibonacci = 1;
+		if (term == 0) {
 			return 0;
-		}
-		else if( term == 1){
+		} else if (term == 1) {
 			return 1;
-		}
-		return 0;
-	}
-	
-	
+		} else {
+			int fibo1 = 1;
+			int fibo2 = 1;
 
+			for (int i = 3; i <= term; i++) {
+
+				fibonacci = fibo1 + fibo2;
+				fibo1 = fibo2;
+				fibo2 = fibonacci;
+			}
+			return fibonacci;
+		}
+
+	}
 }
