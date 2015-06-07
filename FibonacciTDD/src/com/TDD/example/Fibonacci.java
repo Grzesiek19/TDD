bd782b7b75b43727be5854d1d2f1a99a02b2dec7
Grzesiek19 <grzesyou@wp.pl>
2015-06-07 15:17:29 +0200

    Passed test for the expected value for 0 term of Fibonacci sequence

--- FibonacciTDD/src/com/TDD/example/Fibonacci.java
+++ FibonacciTDD/src/com/TDD/example/Fibonacci.java
@@ -2,60 +2,19 @@ package com.TDD.example;
 
 public class Fibonacci {
 
-	private int term;
-
-	public Fibonacci() {
-		this.term = term;
-		// TODO Auto-generated constructor stub
-	}
-	
 	/**
 	 * Gives a value of a Fibonacci sequence for given term.
 	 * 
-	 * @param term, the term for which will be calculated Fibonacci value.
-	 *
-	 * @return         suitable value for given term. 
+	 * @param term, , the term for which will be calculated Fibonacci value.
+	 *             
+	 * @return suitable value for given term.
 	 */
-	
+
 	public static int getFiboValue(int term) {
 		int fibonacci = 1;
 		if (term == 0) {
 			return 0;
-		} else if (term == 1) {
-			return 1;
-		} else {
-			int fibo1 = 1;
-			int fibo2 = 1;
-
-			for (int i = 3; i <= term; i++) {
-
-				fibonacci = fibo1 + fibo2;
-				fibo1 = fibo2;
-				fibo2 = fibonacci;
-			}
-			return fibonacci;
 		}
-
+		return 0;
 	}
-
-	/**
-	 * Gives a sum of a even-valued terms of Fibonacci sequence up to given term
-	 * 
-	 * @param Maxterm, the higher board, to which sum will be calculated
-	 *
-	 * @return       adequate sum. 
-	 */
-	
-	public static int getFiboValuesSum(int MaxTerm) {
-		int FiboSum = 0;
-		for (int i = 0; i <= MaxTerm; i++) {
-
-			if (i % 2 == 0 && getFiboValue(i) <= 4000000)
-				FiboSum += getFiboValue(i);
-
-		}
-		return FiboSum;
-	}
-	
-	
 }
--- FibonacciTDD/src/com/TDD/example/FibonacciTest.java
+++ FibonacciTDD/src/com/TDD/example/FibonacciTest.java
@@ -12,43 +12,5 @@ public class FibonacciTest {
 		int fiboValueFor_0 = fibo.getFiboValue(0);
 		assertEquals(0, fiboValueFor_0);
 	}
-
-	@Test
-	public void testForResultFiboTermFor_1() {
-		Fibonacci fibo = new Fibonacci();
-		int fiboValueFor_1 = fibo.getFiboValue(1);
-		assertEquals(1, fiboValueFor_1);
-	}
-	
-	@Test
-	public void testForResultFiboTermFor_5() {
-		Fibonacci fibo = new Fibonacci();
-		int fiboValueFor_5 = fibo.getFiboValue(5);
-		assertEquals(5, fiboValueFor_5);
-	}
-	
-	@Test
-	public void testForResultFiboTermFor_10() {
-		Fibonacci fibo = new Fibonacci();
-		int fiboValueFor_10 = fibo.getFiboValue(10);
-		assertEquals(55, fiboValueFor_10);
-	}
-	
-	@Test
-	public void testEvenValueTermUntil_4() {
-		Fibonacci fibo = new Fibonacci();
-		assertEquals(4, fibo.getFiboValuesSum(4));
-	}
-	
-	@Test
-	public void testEvenValueTermUntil_10() {
-		Fibonacci fibo = new Fibonacci();
-		assertEquals(88, fibo.getFiboValuesSum(10));
-	}
-	
-	@Test
-	public void testEvenValueTermUntilValueOfTheTerm_4000000() {
-		Fibonacci fibo = new Fibonacci();
-		assertEquals(3524577, fibo.getFiboValuesSum(34));
-	}
 }
+	
